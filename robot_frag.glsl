836c6e7c81375c55c9e324481db7801c4058dfbd
Horácio Macêdo <horacio97@gmail.com>
2022-01-21 03:54:47 -0300

    First commit!
    Hello world! There is nothing done as of now; most of the code comes from another repo that will be properly credited later. The most original thing is the Robot model.

---
+++ robot_frag.glsl
@@ -0,0 +1,29 @@
+#version 300 es
+precision mediump float;
+
+uniform float uShininess;
+uniform vec3 uLightAmbient;
+uniform vec3 uMaterialDiffuse;
+uniform vec3 uMaterialSpecular;
+
+in vec3 vNormal;
+in vec3 vLightRay;
+in vec3 vEyeVector;
+
+out vec4 fragColor;
+
+void main(void) {
+    vec3 L = normalize(vLightRay);
+    vec3 N = normalize(vNormal);
+    float lambertTerm = dot(N, -L);
+    vec3 finalColor = uLightAmbient;
+
+     if (lambertTerm > 0.0) {
+         finalColor += uMaterialDiffuse * lambertTerm;
+         vec3 E = normalize(vEyeVector);
+         vec3 R = reflect(L, N);
+         float specular = pow( max(dot(R, E), 0.0), uShininess);
+         finalColor += uMaterialSpecular * specular;
+     }
+     fragColor = vec4(finalColor, 1.0);
+}
